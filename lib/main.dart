d9a936d8727bf569f3adc088adadc17a4ee2f0c1
Sophie Calixto <96028010+sophiecalixto@users.noreply.github.com>
2023-02-09 13:53:46 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PersonalExpensesApp());
+}
+
+class PersonalExpensesApp extends StatelessWidget {
+  const PersonalExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Despesas Pessoais"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: const [
+          Card(
+            color: Colors.purple,
+            child: Text("Gráfico de gastos"),
+          ),
+          Card(
+            child: Text("Lista de transações"),
+          ),
+        ],
+      ),
+    );
+  }
+}
