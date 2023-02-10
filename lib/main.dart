06cab1c5228ee27e24c1f88e96909094655f19c9
Sophie Calixto <96028010+sophiecalixto@users.noreply.github.com>
2023-02-10 11:30:09 -0300

    feat: Refactor main and remove transaction_user component

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:personal_expenses/components/transaction_user.dart';
+import 'components/transaction_form.dart';
+import 'components/transaction_list.dart';
+import 'models/transaction.dart';
+import 'dart:math';
 
 void main() {
   runApp(const PersonalExpensesApp());
@@ -16,26 +19,80 @@ class PersonalExpensesApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> transactionList = [
+    Transaction(
+      id: 'transacao-1',
+      title: 'Livro Programação Orientada A Objetos',
+      value: 80.0,
+      time: DateTime.now(),
+    ),
+    Transaction(
+      id: 'transacao-2',
+      title: 'Comida Yuki',
+      value: 40.0,
+      time: DateTime.now(),
+    ),
+  ];
+
+  void _addTransaction(String title, double value) {
+    Transaction newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      time: DateTime.now(),
+    );
+
+    setState(() {
+      transactionList.add(newTransaction);
+    });
+  }
+
+  void _openTransactionModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (_) {
+        return TransactionForm(onSubmit: _addTransaction);
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Despesas Pessoais"),
+        actions: [
+          IconButton(
+            onPressed: () => _openTransactionModal(context),
+            icon: const Icon(Icons.add),
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: const [
-            Card(
+          children: [
+            const Card(
               color: Colors.purple,
               child: Text("Gráfico de gastos"),
             ),
-            TransactionUser(),
+            TransactionList(
+              transactionList: transactionList,
+            ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _openTransactionModal(context),
+        child: const Icon(Icons.add),
       ),
     );
   }
